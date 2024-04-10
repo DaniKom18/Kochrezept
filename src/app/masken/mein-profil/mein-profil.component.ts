3c99d2db6b8b53447f5c2d15ff0d7533155d6101
Daniel <Daniel.kom0108@hotmail.com>
2024-04-10 16:51:56 +0200

    init new routerLinks: Leaderboard + Mein-Profil added to Navbar

---
+++ src/app/masken/mein-profil/mein-profil.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mein-profil',
+  standalone: true,
+  imports: [],
+  templateUrl: './mein-profil.component.html',
+  styleUrl: './mein-profil.component.css'
+})
+export class MeinProfilComponent {
+
+}
