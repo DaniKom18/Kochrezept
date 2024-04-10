03513fe97a862f0039f328bf741bc88c7fd747f8
Daniel <Daniel.kom0108@hotmail.com>
2024-04-10 18:01:44 +0200

    RouterLinks hinzugefügt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import {MeineFavoritenComponent} from "./masken/meine-favoriten/meine-favoriten.
 import {MeineRezepteComponent} from "./masken/meine-rezepte/meine-rezepte.component";
 import {StartseiteComponent} from "./masken/startseite/startseite.component";
 import {RezeptDetailViewComponent} from "./masken/rezept-detail-view/rezept-detail-view.component";
+import {MeinProfilComponent} from "./masken/mein-profil/mein-profil.component";
+import {LeaderboardComponent} from "./masken/leaderboard/leaderboard.component";
 
 export const routes: Routes = [
   {path: 'startseite', component: StartseiteComponent },
@@ -11,6 +13,8 @@ export const routes: Routes = [
   {path: 'meine-favoriten', component: MeineFavoritenComponent },
   {path: 'rezept-erstellen', component: RezeptErstellenComponent },
   {path: 'rezept-details/:id', component: RezeptDetailViewComponent },
+  {path: 'profil', component: MeinProfilComponent },
+  {path: 'leaderboard', component: LeaderboardComponent },
   {path: "", redirectTo:"/startseite", pathMatch:"full"},
   {path: "**", redirectTo:"/startseite", pathMatch:"full"},
 ];
