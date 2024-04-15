023ff46343e5d4cf0e53742316cdef87c3f51e4a
Daniel <Daniel.kom0108@hotmail.com>
2024-04-15 11:43:42 +0200

    -Rezept Panel angepasst -Like Btn hinzugefügt -korrektes routing zu rezept-detail-view

--- src/app/shared-components/rezept-panel/rezept-panel.component.ts
+++ src/app/shared-components/rezept-panel/rezept-panel.component.ts
@@ -1,17 +1,32 @@
 import { Component } from '@angular/core';
 import {CardModule} from "primeng/card";
 import {ButtonModule} from "primeng/button";
+import {RatingModule} from "primeng/rating";
+import {FormsModule} from "@angular/forms";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-rezept-panel',
   standalone: true,
   imports: [
     CardModule,
-    ButtonModule
+    ButtonModule,
+    RatingModule,
+    FormsModule,
+    RouterLink
   ],
   templateUrl: './rezept-panel.component.html',
   styleUrl: './rezept-panel.component.css'
 })
 export class RezeptPanelComponent {
 
+  cardInput = [
+    {id: "2", header: "CordonBleau", subheader: "20 min", difficulty: "Leicht", stars: 3, fav: false},
+    {id: "3", header: "Döner", subheader: "20 min", difficulty: "Leicht", stars: 5, fav: true}
+  ]
+
+  setFav(id: string) {
+    this.cardInput.filter(x => x.id == id).map(x => x.fav = !x.fav);
+    console.log(this.cardInput)
+  }
 }
