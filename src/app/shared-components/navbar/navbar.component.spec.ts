5864c07669dd04b5988a67245671e2617a9b1563
Daniel <Daniel.kom0108@hotmail.com>
2024-04-15 12:48:20 +0200

    Navbar Testing hinzugefügt

--- src/app/shared-components/navbar/navbar.component.spec.ts
+++ src/app/shared-components/navbar/navbar.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
+import {RouterTestingModule} from "@angular/router/testing";
+import {By} from "@angular/platform-browser";
+import {RouterLinkWithHref} from "@angular/router";
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -8,10 +11,10 @@ describe('NavbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavbarComponent]
+      imports: [NavbarComponent, RouterTestingModule.withRoutes([])]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +22,24 @@ describe('NavbarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have links with correct routerLinks', () => {
+    const links = fixture.debugElement.queryAll(By.directive(RouterLinkWithHref));
+    const linkDes = links.map(de => de.injector.get(RouterLinkWithHref));
+    expect(linkDes[0].href).toContain('/startseite');
+    expect(linkDes[1].href).toContain('/startseite');
+    expect(linkDes[2].href).toContain('/meine-rezepte');
+    expect(linkDes[3].href).toContain('/meine-favoriten');
+    expect(linkDes[4].href).toContain('/rezept-erstellen');
+    expect(linkDes[5].href).toContain('/leaderboard');
+    expect(linkDes[6].href).toContain('/profil');
+    expect(linkDes[7].href).toContain('/abmelden');
+  });
+
+  it('should display logo image correctly', () => {
+    const logoImg = fixture.debugElement.query(By.css('li.nav-item a img'));
+    expect(logoImg.nativeElement.src).toContain('assets/Logo.png');
+    expect(logoImg.nativeElement.style.width).toBe('85px');
   });
 });
