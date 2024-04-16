d10f608d88b2e392d085b4a14120768a007d207c
Daniel <Daniel.kom0108@hotmail.com>
2024-04-16 17:38:12 +0200

    erstes Profil-Layout

--- src/app/masken/mein-profil/mein-profil.component.ts
+++ src/app/masken/mein-profil/mein-profil.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import {ButtonModule} from "primeng/button";
+import {ProgressBarModule} from "primeng/progressbar";
 
 @Component({
   selector: 'app-mein-profil',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonModule,
+    ProgressBarModule
+  ],
   templateUrl: './mein-profil.component.html',
   styleUrl: './mein-profil.component.css'
 })
 export class MeinProfilComponent {
+  user = {
+    username: 'JohnDoe',
+    email: 'john.doe@example.com',
+    xp: 75,
+    level: 5
+  };
 
+  onEdit() {
+    // Logik zum Bearbeiten der Benutzerdaten
+  }
+
+  onDelete() {
+    // Logik zum Löschen des Benutzerkontos
+    if (confirm("Are you sure you want to delete your account? This cannot be undone.")) {
+      // Account löschen Logik
+    }
+  }
 }
