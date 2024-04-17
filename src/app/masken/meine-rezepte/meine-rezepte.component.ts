eae6d19bcf9ab25d98211eed524a717f4456230b
Daniel <Daniel.kom0108@hotmail.com>
2024-04-17 15:14:18 +0200

    Routing hinzugefügt für einzelene Rezepte

--- src/app/masken/meine-rezepte/meine-rezepte.component.ts
+++ src/app/masken/meine-rezepte/meine-rezepte.component.ts
@@ -5,8 +5,10 @@ import {PaginatorModule} from "primeng/paginator";
 import {InputTextModule} from "primeng/inputtext";
 import {RippleModule} from "primeng/ripple";
 import {NgClass} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 interface Rezept{
+  id: number,
   name: string,
   image: string,
   rating: number,
@@ -20,15 +22,16 @@ interface Rezept{
     PaginatorModule,
     InputTextModule,
     RippleModule,
-    NgClass
+    NgClass,
+    RouterLink
   ],
   templateUrl: './meine-rezepte.component.html',
   styleUrl: './meine-rezepte.component.css'
 })
 export class MeineRezepteComponent {
-  products:Rezept[] = [
-    {name: 'Lebkuchen', image: 'https://images.pexels.com/photos/906054/pexels-photo-906054.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2', rating: 3.4},
-    {name: 'Döner', image: 'https://images.pexels.com/photos/15202777/pexels-photo-15202777/free-photo-of-mahlzeit-fleisch-frisch-essensfotografie.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2', rating: 5.0}
+  recipes:Rezept[] = [
+    {id: 4, name: 'Lebkuchen', image: 'https://images.pexels.com/photos/906054/pexels-photo-906054.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2', rating: 3.4},
+    {id: 3,name: 'Döner', image: 'https://images.pexels.com/photos/15202777/pexels-photo-15202777/free-photo-of-mahlzeit-fleisch-frisch-essensfotografie.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2', rating: 5.0}
   ]
 
   editProduct(product: any) {
