de36deaf1b0175bec859a9ee376014cf3e075855
Daniel <Daniel.kom0108@hotmail.com>
2024-04-17 15:29:33 +0200

    Interfaces global gemacht

---
+++ src/app/models/recipe.ts
@@ -0,0 +1,10 @@
+import {Ingredient} from "./ingredient";
+
+export interface Recipe {
+  id: number,
+  name:string,
+  preparation:string,
+  ingredients:Ingredient[],
+  image:string,
+  rating: number,
+}
