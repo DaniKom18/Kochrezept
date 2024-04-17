e05fd85c1ac2655c5a71d9e56b6a4da5ae3e613d
Emre Gezer <emregezer2404@gmail.com>
2024-04-17 13:50:45 +0200

    Ui-Design -> Meine Rezepte

--- src/app/masken/meine-rezepte/meine-rezepte.component.ts
+++ src/app/masken/meine-rezepte/meine-rezepte.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import {TableModule} from "primeng/table";
+import {ButtonModule} from "primeng/button";
+import {PaginatorModule} from "primeng/paginator";
+import {InputTextModule} from "primeng/inputtext";
+import {RippleModule} from "primeng/ripple";
+import {NgClass} from "@angular/common";
 
+interface Rezept{
+  name: string,
+  image: string,
+}
 @Component({
   selector: 'app-meine-rezepte',
   standalone: true,
-  imports: [],
+  imports: [
+    TableModule,
+    ButtonModule,
+    PaginatorModule,
+    InputTextModule,
+    RippleModule,
+    NgClass
+  ],
   templateUrl: './meine-rezepte.component.html',
   styleUrl: './meine-rezepte.component.css'
 })
 export class MeineRezepteComponent {
+  products:Rezept[] = [{name: 'Lebkuchen', image: 'lebkuchen'}]
 
+  editProduct(product: any) {
+
+  }
+
+  deleteProduct(product: any) {
+
+  }
+
+  toggleVisibility(product: any) {
+
+  }
+
+  toggleAnonym(product: any) {
+
+  }
 }
