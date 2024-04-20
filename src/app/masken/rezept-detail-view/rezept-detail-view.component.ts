bf2ae2bc49367fa3b0a04d069cab4b4235cbe069
MekiAnjo <39812088+MekiAnjo@users.noreply.github.com>
2024-04-20 18:16:03 +0200

    kommentar section hinzugefügt, f css

--- src/app/masken/rezept-detail-view/rezept-detail-view.component.ts
+++ src/app/masken/rezept-detail-view/rezept-detail-view.component.ts
@@ -1,11 +1,15 @@
 import {Component, Input} from '@angular/core';
 import {ButtonModule} from "primeng/button";
+import {Comment} from "../../models/comment";
+import {Feedback} from "../../models/feedback";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-rezept-detail-view',
   standalone: true,
   imports: [
-    ButtonModule
+    ButtonModule,
+    NgForOf
   ],
   templateUrl: './rezept-detail-view.component.html',
   styleUrl: './rezept-detail-view.component.css'
@@ -14,7 +18,26 @@ export class RezeptDetailViewComponent {
   recipeId: number = -1;
 
   @Input() //Mapped id die durch die URL übergeben wurde auf recipeId
-  set id(value: number){
+  set id(value: number) {
     this.recipeId = value;
+  }
+
+  comments: Comment[] = [
+    {
+      id: 1,
+      text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    },
+    {
+      id: 2,
+      text: "At vero eos et accusam et justo duo dolores et ea rebum.",
+    }
+  ];
+
+  feedback: Feedback = {
+    id: '1',
+    comment: this.comments,
+    rating: '5',
+    recipeId: this.recipeId,
+    userId: 'uuid-13-asd-e'
   }
 }
