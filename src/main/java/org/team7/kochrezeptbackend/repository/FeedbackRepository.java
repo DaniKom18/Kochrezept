b8941ca3fe3073f7bd24e0bba8a39702bf5e98a9
Daniel <Daniel.kom0108@hotmail.com>
2024-04-22 18:32:45 +0200

    Feedback angepasst

--- src/main/java/org/team7/kochrezeptbackend/repository/FeedbackRepository.java
+++ src/main/java/org/team7/kochrezeptbackend/repository/FeedbackRepository.java
@@ -4,6 +4,10 @@ import org.team7.kochrezeptbackend.entity.Feedback;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
+
 @Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+    List<Feedback> findByIdIn(Set<Long> feedbackIds);
 }
