890f55147a7d9c3d620b6324b5e733dde3a5a604
Emre Gezer <emregezer2404@gmail.com>
2024-04-22 16:25:46 +0200

    Created Ingredient and changed quantity from double to -> Double

--- src/main/java/org/team7/kochrezeptbackend/service/IngredientService.java
+++ src/main/java/org/team7/kochrezeptbackend/service/IngredientService.java
@@ -1,4 +1,13 @@
 package org.team7.kochrezeptbackend.service;
 
+import org.team7.kochrezeptbackend.entity.Ingredient;
+import java.util.List;
+import java.util.Optional;
+
 public interface IngredientService {
+    Ingredient saveIngredient(Ingredient ingredient);
+    Optional<Ingredient> getIngredientById(Long id);
+    List<Ingredient> getAllIngredient();
+    Ingredient updateIngredient(Ingredient ingredient);
+    void deleteIngredient(Long id);
 }
--- src/main/java/org/team7/kochrezeptbackend/service/impl/IngredientServiceImpl.java
+++ src/main/java/org/team7/kochrezeptbackend/service/impl/IngredientServiceImpl.java
@@ -1,4 +1,51 @@
 package org.team7.kochrezeptbackend.service.impl;
 
-public class IngredientServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.team7.kochrezeptbackend.entity.Ingredient;
+import org.team7.kochrezeptbackend.repository.IngredientRepository;
+import org.team7.kochrezeptbackend.service.IngredientService;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class IngredientServiceImpl implements IngredientService {
+
+    private final IngredientRepository ingredientRepository;
+
+    @Autowired
+    public IngredientServiceImpl(IngredientRepository ingredientRepository) { this.ingredientRepository = ingredientRepository; }
+
+    @Override
+    @Transactional
+    public Ingredient saveIngredient(Ingredient ingredient) { return ingredientRepository.save(ingredient); }
+
+    @Override
+    public Optional<Ingredient> getIngredientById(Long id) { return ingredientRepository.findById(id); }
+
+    @Override
+    public List<Ingredient> getAllIngredient() {
+        return ingredientRepository.findAll();
+    }
+
+    @Override
+    public Ingredient updateIngredient(Ingredient updatedIngredient) {
+        return ingredientRepository.findById(updatedIngredient.getId())
+                .map(existingIngredient -> {
+                    if (updatedIngredient.getName() != null) existingIngredient.setName(updatedIngredient.getName());
+                    if (updatedIngredient.getQuantity() != null) existingIngredient.setQuantity(updatedIngredient.getQuantity());
+                    if (updatedIngredient.getMeasure() != null) existingIngredient.setMeasure(updatedIngredient.getMeasure());
+                    if (updatedIngredient.getRecipe() != null) existingIngredient.setRecipe(updatedIngredient.getRecipe());
+                    return ingredientRepository.save(existingIngredient);
+                })
+                .orElseThrow(() -> new RuntimeException("Ingredient not found with id:" + updatedIngredient.getId()));
+    }
+
+    @Override
+    public void deleteIngredient(Long id) {
+        ingredientRepository.deleteById(id);
+    }
 }
+
