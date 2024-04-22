fb58daff7a5a9fd73981e6408f5de3a5be0fa671
Daniel <Daniel.kom0108@hotmail.com>
2024-04-22 13:15:16 +0200

    init entities

---
+++ src/main/java/org/team7/kochrezeptbackend/entity/User.java
@@ -0,0 +1,42 @@
+package org.team7.kochrezeptbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column
+    private String email;
+
+    @Column(nullable = false)
+    private double xp;
+
+    @Column(nullable = false)
+    private int level;
+
+    @Column
+    private Integer countOfUploadedRecipes;
+
+    @Column
+    private Integer countOfFavoriteRecipes;
+
+    @ElementCollection
+    @CollectionTable(name = "user_favorite_recipes", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "recipe_id")
+    private Set<Integer> favRecipes;
+}
