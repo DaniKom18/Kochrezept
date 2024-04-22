e086715a8f59a8787d09648eb9a14af6b9900e80
Daniel <Daniel.kom0108@hotmail.com>
2024-04-22 18:20:04 +0200

    Comment Service Impl

---
+++ src/main/java/org/team7/kochrezeptbackend/service/impl/CommentServiceImpl.java
@@ -0,0 +1,34 @@
+package org.team7.kochrezeptbackend.service.impl;
+
+import org.team7.kochrezeptbackend.entity.Comment;
+import org.team7.kochrezeptbackend.repository.CommentRepository;
+import org.team7.kochrezeptbackend.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+
+    private final CommentRepository commentRepository;
+
+    @Autowired
+    public CommentServiceImpl(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    @Override
+    @Transactional
+    public Comment saveComment(Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Comment> getCommentsByIds(Set<Long> commentIds) {
+        return commentRepository.findByIdIn(commentIds);
+    }
+}
