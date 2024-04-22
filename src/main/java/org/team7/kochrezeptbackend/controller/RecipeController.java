a27c8215f6a537544f95c9523c883fff8945378a
Emre Gezer <emregezer2404@gmail.com>
2024-04-22 16:05:00 +0200

    RecipeController Created

---
+++ src/main/java/org/team7/kochrezeptbackend/controller/RecipeController.java
@@ -0,0 +1,65 @@
+package org.team7.kochrezeptbackend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.team7.kochrezeptbackend.entity.Recipe;
+import org.team7.kochrezeptbackend.service.RecipeService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/recipes/")
+public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    @Autowired
+    public RecipeController(RecipeService recipeService) { this.recipeService = recipeService; }
+
+    @PostMapping
+    public ResponseEntity<Recipe> createRecipe(@RequestBody Recipe recipe){
+        Recipe savedRecipe = recipeService.saveRecipe(recipe);
+        return new ResponseEntity<>(savedRecipe, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Recipe> getRecipeById(@PathVariable Long id){
+        Optional<Recipe> recipe = recipeService.getRecipeById(id);
+        return recipe.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Recipe>> getAllRecipe(){
+        List<Recipe> recipes = recipeService.getAllRecipe();
+        return new ResponseEntity<>(recipes, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Recipe> updateRecipe(@PathVariable Long id, @RequestBody  Recipe recipe){
+        if (!recipe.getId().equals(id)){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Optional<Recipe> existingRecipe = recipeService.getRecipeById(id);
+        if (existingRecipe.isPresent()){
+            Recipe updaRecipe = recipeService.updateRecipe(recipe);
+            return new ResponseEntity<>(updaRecipe, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteRecipe(@PathVariable Long id){
+        Optional<Recipe> recipe = recipeService.getRecipeById(id);
+        if (recipe.isPresent()){
+            recipeService.deleteRecipe(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+}
