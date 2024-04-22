a948751d65a3abeaa6423c2862e5a08e63d0d3f9
Daniel <Daniel.kom0108@hotmail.com>
2024-04-22 18:20:11 +0200

    Comment Controller

---
+++ src/main/java/org/team7/kochrezeptbackend/controller/CommentController.java
@@ -0,0 +1,38 @@
+package org.team7.kochrezeptbackend.controller;
+
+import org.team7.kochrezeptbackend.entity.Comment;
+import org.team7.kochrezeptbackend.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/api/comments")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @Autowired
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Comment> createComment(@RequestBody Comment comment) {
+        Comment savedComment = commentService.saveComment(comment);
+        return new ResponseEntity<>(savedComment, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/byIds")
+    public ResponseEntity<List<Comment>> getCommentsByIds(@RequestParam Set<Long> ids) {
+        List<Comment> comments = commentService.getCommentsByIds(ids);
+        if (comments.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(comments, HttpStatus.OK);
+    }
+}
