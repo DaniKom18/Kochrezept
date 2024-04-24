d2233593d5481cc3de0725d022151fbbd65ddfd3
Daniel <Daniel.kom0108@hotmail.com>
2024-04-24 18:42:51 +0200

    Anuglar zu Keycloak anbindung hinzugefügt

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,57 @@
-import { ApplicationConfig } from '@angular/core';
+import {APP_INITIALIZER, ApplicationConfig, Provider} from '@angular/core';
 import {provideRouter, withComponentInputBinding} from '@angular/router';
 
 import { routes } from './app.routes';
 import {provideAnimations} from "@angular/platform-browser/animations";
+import {KeycloakBearerInterceptor, KeycloakService} from "keycloak-angular";
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
+
+// Function to initialize Keycloak with the necessary configurations
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      // Configuration details for Keycloak
+      config: {
+        url: 'http://localhost:8080', // URL of the Keycloak server
+        realm: 'myRealm', // Realm to be used in Keycloak
+        clientId: 'kochrezept-client' // Client ID for the application in Keycloak
+      },
+      // Options for Keycloak initialization
+      initOptions: {
+        onLoad: 'login-required', // Action to take on load
+       // silentCheckSsoRedirectUri:
+       //   window.location.origin + '/assets/silent-check-sso.html' // URI for silent SSO checks
+      },
+      // Enables Bearer interceptor
+      enableBearerInterceptor: true,
+      // Prefix for the Bearer token
+      bearerPrefix: 'Bearer',
+      // URLs excluded from Bearer token addition (empty by default)
+      //bearerExcludedUrls: []
+    });
+}
+
+// Provider for Keycloak Bearer Interceptor
+const KeycloakBearerInterceptorProvider: Provider = {
+  provide: HTTP_INTERCEPTORS,
+  useClass: KeycloakBearerInterceptor,
+  multi: true
+};
+
+// Provider for Keycloak Initialization
+const KeycloakInitializerProvider: Provider = {
+  provide: APP_INITIALIZER,
+  useFactory: initializeKeycloak,
+  multi: true,
+  deps: [KeycloakService]
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withInterceptorsFromDi()), // Provides HttpClient with interceptors
+    KeycloakInitializerProvider, // Initializes Keycloak
+    KeycloakBearerInterceptorProvider, // Provides Keycloak Bearer Interceptor
+    KeycloakService, // Service for Keycloak
     provideRouter(routes, withComponentInputBinding()),
     provideAnimations(),
   ]
