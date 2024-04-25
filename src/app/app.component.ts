702140e6a6141c81f08abce7be8b27369ffe40fb
Daniel <Daniel.kom0108@hotmail.com>
2024-04-25 23:22:49 +0200

    Extrahierung der UUID des Users von keycloak. UUID wird global hinterlegt. Falls User noch nicht im Backend existiert wird ein neuer angelegt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from "./shared-components/navbar/navbar.component";
 import {KeycloakService} from "keycloak-angular";
 import {KeycloakProfile} from "keycloak-js";
+import {UserService} from "./services/user.service";
+import {userSession} from "../environments/user-uuid";
+import {User} from "./models/user";
 
 @Component({
   selector: 'app-root',
@@ -15,13 +18,30 @@ export class AppComponent implements OnInit{
   public isLoggedIn = false;
   public userProfile: KeycloakProfile | null = null;
 
-  constructor(private readonly keycloak: KeycloakService) {}
+  constructor(private readonly keycloak: KeycloakService,
+              private userservice: UserService) {}
 
+  // Erste Überprüfung ob der User exisitiert
+  // Wenn nicht wird durch diesen Aufruf der User in der Datenbank angelegt
   public async ngOnInit() {
     this.isLoggedIn = this.keycloak.isLoggedIn();
 
     if (this.isLoggedIn) {
       this.userProfile = await this.keycloak.loadUserProfile();
+      userSession.id = this.userProfile.id!; // User id wird global gespeichert
+
+      const user: User = {
+        id: this.userProfile.id!,
+        username: this.userProfile.username!,
+        email: this.userProfile.email!,
+      }
+
+      // Falls user schon existiert wird er im Backend nicht nochmal gespeichert
+      this.userservice.saveUser(user).subscribe(
+        data => {
+          console.log(data)
+        }
+      )
     }
   }
 }
