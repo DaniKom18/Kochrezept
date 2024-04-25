99cc392d16a57326efaa031be68740d8a39d25e5
Daniel <Daniel.kom0108@hotmail.com>
2024-04-25 23:23:43 +0200

    UserService

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {User} from "../models/user";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  baseUrl: string = environment.baseUrl + "/api/users"
+
+  constructor(private httpClient: HttpClient) { }
+
+  getUserByUuid(uuid: string) {
+    const url: string = `${this.baseUrl}/${uuid}`;
+    return this.httpClient.get<User>(url)
+  }
+
+  saveUser(user: User) {
+    const url: string = this.baseUrl;
+    return this.httpClient.post<any>(url, user)
+  }
+}
