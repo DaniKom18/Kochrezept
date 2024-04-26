4c7959783ca3a72e1d406caa9d0cd71635cfa8b9
Daniel <Daniel.kom0108@hotmail.com>
2024-04-26 12:23:27 +0200

    query für leaderboard hinzugefügt

--- src/main/java/org/team7/kochrezeptbackend/repository/UserRepository.java
+++ src/main/java/org/team7/kochrezeptbackend/repository/UserRepository.java
@@ -1,12 +1,17 @@
 package org.team7.kochrezeptbackend.repository;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.team7.kochrezeptbackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     // Benutzerdefinierte Abfragen für Benutzer können hier definiert werden
+    @Query("SELECT u FROM User u ORDER BY u.level DESC, u.xp DESC")
+    List<User> findTop10ByOrderByLevelDesc(Pageable pageable);
 }
