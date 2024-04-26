182f340a70ba5ef9dfaf69f947250dd70a41bc4b
Daniel <Daniel.kom0108@hotmail.com>
2024-04-26 14:41:49 +0200

    -added RequestIds -changed Get To Post

---
+++ src/main/java/org/team7/kochrezeptbackend/request/RequestIds.java
@@ -0,0 +1,12 @@
+package org.team7.kochrezeptbackend.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+public class RequestIds {
+    Set<Long> ids;
+}
