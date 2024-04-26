fdc06c3fc472e8ef33766e11e8c59bfeef7bd05a
Daniel <Daniel.kom0108@hotmail.com>
2024-04-26 12:20:05 +0200

    leaderboard fertig gestellt

--- src/app/masken/leaderboard/leaderboard.component.ts
+++ src/app/masken/leaderboard/leaderboard.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {User} from "../../models/user";
+import {UserService} from "../../services/user.service";
 
 @Component({
   selector: 'app-leaderboard',
@@ -8,19 +9,22 @@ import {User} from "../../models/user";
   templateUrl: './leaderboard.component.html',
   styleUrl: './leaderboard.component.css'
 })
-export class LeaderboardComponent {
-  users: User[] = [
-    {username: "Emre", level: 10,  xp: 0.3},
-    {username: "Dani", level: 5,  xp: 0.6},
-    {username: "Lev oi", level: 5,  xp: 0.4},
-    {username: "Kevin", level: 3,  xp: 0.6},
-    {username: "ldpad", level: 2,  xp: 0.3},
-    {username: "wkdoaw", level: 1,  xp: 0.8},
-    {username: "dsd", level: 1,  xp: 0.6},
-    {username: "kloda", level: 1,  xp: 0.3},
-    {username: "lp", level: 1,  xp: 0.87},
-    {username: "ddd", level: 1,  xp: 0.2},
-  ]
+export class LeaderboardComponent implements OnInit{
+
+
+  constructor(private userservice: UserService) {
+  }
+
+  users: User[] = []
+
+  ngOnInit(): void {
+    this.userservice.getLeaderboard().subscribe(
+      data => {
+        this.users = data
+      }
+    )
+  }
+
 
 
 }
