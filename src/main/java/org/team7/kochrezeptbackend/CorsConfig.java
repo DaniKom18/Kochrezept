687d027e6fa76dbb4ced87deaf9b63e9a12c6a04
Daniel <Daniel.kom0108@hotmail.com>
2024-04-27 09:35:03 +0200

    Verfickte Cors scheiße endlich gelößt

---
+++ src/main/java/org/team7/kochrezeptbackend/CorsConfig.java
@@ -0,0 +1,14 @@
+package org.team7.kochrezeptbackend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+    }
+}
