d4017bc13738ab19204c9954edcc373f9fd18b3c
MekiAnjo <39812088+MekiAnjo@users.noreply.github.com>
2024-04-30 19:43:52 +0200

    comment und feedback services hinzugefügt

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Comment} from "../models/comment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
 
-  constructor() { }
+  private baseUrl: string = environment.baseUrl + "/api"
+
+  private headers = new HttpHeaders({'Content-Type': 'application/json', 'accept': 'application/json'});
+  private options = {headers: this.headers};
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getAllCommentsOfFeedback(feedbackId: number) {
+    const url = this.baseUrl + "/feedback/" + feedbackId + "/comments"
+    console.log("Getting Feedback over this url: " + url)
+    return this.httpClient.get<Comment[]>(url)
+  }
+
+  saveComment(commentOfFeedback: Comment, feedbackId: number) {
+    console.log("Save Comment On Feedback:=" + feedbackId)
+    const url = this.baseUrl + "/feedback/" + feedbackId + "/comments"
+    return this.httpClient.post(url, commentOfFeedback, this.options)
+  }
 }
