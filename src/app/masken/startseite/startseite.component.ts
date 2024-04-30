92d4e894515c5d5daeeb282cfc98324281f74e2d
Daniel <Daniel.kom0108@hotmail.com>
2024-04-30 16:30:32 +0200

    Added Messages

--- src/app/masken/startseite/startseite.component.ts
+++ src/app/masken/startseite/startseite.component.ts
@@ -7,6 +7,7 @@ import {Ingredient} from "../../models/ingredient";
 import {Recipe} from "../../models/recipe";
 import {RecipeService} from "../../services/recipe.service";
 import {UserService} from "../../services/user.service";
+import {MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-startseite',
@@ -24,7 +25,8 @@ export class StartseiteComponent implements OnInit {
 
 
   constructor(private recipeService: RecipeService,
-              private userService: UserService) {
+              private userService: UserService,
+              private messageService: MessageService) {
   }
 
   allRecipes: Recipe[] = []; // Behält alle Rezepte
@@ -47,6 +49,7 @@ export class StartseiteComponent implements OnInit {
   }
 
   favEvent(recipe: Recipe) {
+    this.messageService.add({ severity: 'success', summary: 'Erfolgreich', detail: 'Rezept wurde Erfolgreich zu deinen Favoriten hinzugefügt' });
     this.filteredRecipes = this.filteredRecipes.filter(r => r.id != recipe.id);
     this.recipeService.userClickedRecipeAsFav(recipe).subscribe()
   }
