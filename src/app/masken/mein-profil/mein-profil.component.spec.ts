3321b4457f2075ccdc4b6be2511ff366bf334574
Emre Gezer <emregezer2404@gmail.com>
2024-05-06 15:43:44 +0200

    Test Coverage bei "Mein Profil" mit Codium

--- src/app/masken/mein-profil/mein-profil.component.spec.ts
+++ src/app/masken/mein-profil/mein-profil.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MeinProfilComponent } from './mein-profil.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {of} from "rxjs";
+import {userSession} from "../../../environments/user-uuid";
 
 describe('MeinProfilComponent', () => {
   let component: MeinProfilComponent;
@@ -8,10 +11,10 @@ describe('MeinProfilComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MeinProfilComponent]
+      imports: [MeinProfilComponent, HttpClientTestingModule]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(MeinProfilComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +23,25 @@ describe('MeinProfilComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize component successfully', async () => {
+    // Arrange
+    const userServiceMock = jasmine.createSpyObj('UserService', ['waitForUserSession', 'getUserByUuid']);
+    userServiceMock.waitForUserSession.and.returnValue(Promise.resolve());
+    userServiceMock.getUserByUuid.and.returnValue(of({ username: 'testUser', xp: 100, level: 2 }));
+
+    const recipeServiceMock = jasmine.createSpyObj('RecipeService', ['getRecipesOfUser']);
+    recipeServiceMock.getRecipesOfUser.and.returnValue(of([]));
+
+    const meinProfilComponent = new MeinProfilComponent(userServiceMock, recipeServiceMock);
+
+    // Act
+    await meinProfilComponent.ngOnInit(); // Verwende async/await, um ngOnInit korrekt zu testen
+
+    // Assert
+    expect(userServiceMock.waitForUserSession).toHaveBeenCalled();
+    expect(userServiceMock.getUserByUuid).toHaveBeenCalledWith(userSession.id);
+    expect(recipeServiceMock.getRecipesOfUser).toHaveBeenCalled();
+  });
+
 });
